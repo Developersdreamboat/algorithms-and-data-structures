c8b52ae94303918cd181e5907c14b6574ec66474
Evgenii Pavlenko <evgenii.pavlenko.developer@gmail.com>
2023-03-13 15:22:57 +0200

    Add inOrder, preOrder, postOrder traversals

--- data-structures/balancedBST.js
+++ data-structures/balancedBST.js
@@ -156,15 +156,126 @@ class Tree {
       return array;
     }
   }
+
+  inOrder(callback) {
+    if (this.root === null) {
+      return null;
+    }
+  
+    const array = [];
+    const stack = [];
+    let current = this.root;
+  
+    while (stack.length !== 0 || current !== null) {
+      if (current !== null) {
+        stack.push(current);
+        current = current.left;
+      } else {
+        current = stack.pop();
+        array.push(current);
+        current = current.right;
+      }
+    }
+
+    if (callback !== undefined) {
+      for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+      }
+
+      return;
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].data;
+      }
+
+      return array;
+    }
+  }
+  
+  preOrder(callback) {
+    if (this.root === null) {
+      return null;
+    }
+  
+    const array = [];
+    const stack = [];
+    stack.push(this.root);
+  
+    while (stack.length !== 0) {
+      let current = stack.pop();
+      array.push(current);
+  
+      if (current.right !== null) {
+        stack.push(current.right);
+      }
+  
+      if (current.left !== null) {
+        stack.push(current.left);
+      }
+    }
+  
+    if (callback !== undefined) {
+      for (let i = 0; i < array.length; i++) {
+        callback(array[i]);
+      }
+  
+      return;
+    } else {
+      for (let i = 0; i < array.length; i++) {
+        array[i] = array[i].data;
+      }
+  
+      return array;
+    }
+  }
+
+  postOrder(callback) {
+    if (this.root === null) {
+      return null;
+    }
+    
+    const array = [];
+    const stackPreOrder = [];
+    const stackPostOrder = [];
+    let current = this.root;
+  
+    stackPreOrder.push(current);
+  
+    while (stackPreOrder.length !== 0) {
+      current = stackPreOrder.pop();
+      stackPostOrder.push(current);
+  
+      if (current.left !== null) {
+        stackPreOrder.push(current.left);
+      }
+  
+      if (current.right !== null) {
+        stackPreOrder.push(current.right);
+      }
+    }
+  
+    if (callback !== undefined) {
+      for (let i = stackPostOrder.length - 1; i >= 0; i--) {
+        callback(stackPostOrder[i]);
+      }
+      return;
+    } else {
+      for (let i = stackPostOrder.length - 1; i >= 0; i--) {
+        array.push(stackPostOrder[i].data);
+      }
+      return array;
+    }
+  }
 } 
 
 
 
 /* Driver */
-/* const array = [5, 1, 10, 2, 1, 8, 4, 5, 3, 5];
+const array = [5, 1, 10, 2, 1, 8, 4, 5, 3, 5];
 const sortedArray = mergeSort(array);
 const buildArray = removeDuplicatesFromSortedArray(sortedArray);
-let tree = new Tree(buildArray); */
+let tree = new Tree(buildArray);
+prettyPrint(tree.root);
 
 /* Insertion */
 /* tree.insert(7);
@@ -178,8 +289,14 @@ prettyPrint(tree.root); */
 /* console.log(tree.find(5)); */
 
 /* Test with provided function and without */
-/* function printNodeValue(node) {
+function printNodeValue(node) {
   console.log(node.data);
 }
-tree.levelOrder(printNodeValue);
+/* tree.levelOrder(printNodeValue);
 console.log(tree.levelOrder()); */
+/* tree.inOrder(printNodeValue);
+console.log(tree.inOrder()); */
+/* tree.preOrder(printNodeValue);
+console.log(tree.preOrder()); */
+tree.postOrder(printNodeValue);
+console.log(tree.postOrder());
